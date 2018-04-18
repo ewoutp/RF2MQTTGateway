8d05d5489bf659bbd76ae9947810b9244cfe6168
Ewout Prangsma <ewout@prangsma.net>
2018-04-18 09:01:32 +0200

    Added secrets template

---
+++ src/secrets-template.h
@@ -0,0 +1,8 @@
+// Fill your secrets and rename this file to secrets.h
+#pragma once
+
+#define WIFI_SSID "<your SSID>"
+#define WIFI_PASS "<your WIFI password>"
+
+#define MQTT_HOST IPAddress(192,168,1,10) // Your MQTT Server IP 
+#define MQTT_PORT 1883
